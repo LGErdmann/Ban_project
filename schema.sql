03dcc96c26e6073e8ff84febb2ff804248f4c2de
daemonlucas1999 <daemonlucas@gmail.com>
2023-11-23 10:16:51 -0300

    Tabela Agendamento

--- schema.sql
+++ schema.sql
@@ -23,7 +23,7 @@ CREATE TABLE IF NOT EXISTS "MedVet"(
 
 
 CREATE TABLE IF NOT EXISTS "Consulta"(
-    "CodCOnsulta" SERIAL PRIMARY KEY NOT NULL,
+    "CodConsulta" SERIAL PRIMARY KEY NOT NULL,
     "ObsVet" VARCHAR(100) NOT NULL,
     "PrescVet" VARCHAR(100) NOT NULL   
 
@@ -38,14 +38,22 @@ CREATE TABLE IF NOT EXISTS "Animais"(
     "Nome" VARCHAR(20) NOT NULL,
     "idAnimal" INTEGER NULL,
     "TamanhoBox" VARCHAR(20) NULL,
-
-
     
-
 );
 
 
 CREATE TABLE IF NOT EXISTS "Agendamento"(
+    "NumProtocolo" SERIAL PRIMARY KEY NOT NULL,
+    "CodConsulta" INTEGER PRIMARY KEY NOT NULL,
+    "IdFunc" INTEGER NOT NULL,
+    "DataAgend" VARCHAR(20) NOT NULL,
+    "HoraAgend" VARCHAR(10) NOT NULL,
+    
+
+    FOREIGN KEY ("CodConsulta") REFERENCES "Consulta",
+    FOREIGN KEY ("IdFunc") REFERENCES "Funcionarios"
+
+
 
 );
 
