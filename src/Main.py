b44da534b773e72aad1a35bf2df17bfb7b7adcf5
daemonlucas1999 <daemonlucas@gmail.com>
2023-11-25 17:41:00 -0300

    Conexão/Select/Insert

--- src/Main.py
+++ src/Main.py
@@ -1,20 +1,51 @@
 import streamlit as st
-import os
-import sqlalchemy
 import pandas as pd
 import psycopg2
 
-
-
 # Initialize connection.
-
-
 @st.cache_resource
-def get_cenct():
-    return st.connection("postgres", type="sql")
+def get_conn():
+    conn = psycopg2.connect(
+        host='localhost',
+        port='5432',
+        database='postgres',
+        user='postgres',
+        password='postgres'
+    )
+    return conn
 
-conn = get_cenct()
-Nome = "Funcionarios"
-df = conn.query(f'SELECT * FROM "{Nome}";', ttl="10m")
-st.dataframe(df)
-conn.query
+conn = get_conn()
+
+colTetx, colButton = st.columns(2)
+with colTetx:
+    Nome = st.text_input(label="teste")
+with colButton:
+    buttonSelectAll = st.button(label="Pesquisar")
+
+if buttonSelectAll:
+    cur = conn.cursor()
+    cur.execute(f'SELECT * FROM "{Nome}"')
+    results = cur.fetchall()
+    st.dataframe(results)
+    cur.close()
+
+
+colWriteInsert, colButtonInsert = st.columns(2)
+
+with colWriteInsert:
+    Cargo = st.text_input(label="Cargo")
+    Setor = st.text_input(label="Setor")
+    Telefone = st.text_input(label="Telefone")
+    Email = st.text_input(label="Email")
+    Salario = st.number_input(label="Salário", value=None, placeholder="Sálario")
+    Nome = st.text_input(label="Nome")
+    _returnData = [Cargo, Setor, Telefone, Email, Salario, Nome]
+with colButtonInsert:
+    buttonSelectAll = st.button(label="Inserir")
+
+
+if buttonSelectAll:
+    cur = conn.cursor()
+    cur.execute(f'INSERT INTO "Funcionarios" ("Cargo", "Setor", "Telefone", "Email", "Salário", "Nome") VALUES ' \
+            f'(\'{Cargo}\', \'{Setor}\', \'{Telefone}\', \'{Email}\', {Salario}, \'{Nome}\')')
+    cur.close
